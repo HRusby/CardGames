aa506d474642c4d6fd86a852eeeb481b900b4f7f
HRusby <harrygeorger@hotmail.com>
2023-09-03 16:28:09 +0100

    Testing Equality on Decks

--- CardGames.Base.Tests/DeckTests.cs
+++ CardGames.Base.Tests/DeckTests.cs
@@ -5,14 +5,68 @@ public class DeckTests
     [Fact]
     public void TestStandardDeck()
     {
-       IEnumerable<ACard> standardDeck = Deck.BuildStandardDeck().Cards;
+      int aceHighValue = 15;
+      IEnumerable<ACard> standardDeck = Deck.BuildStandardDeck(aceHighValue).Cards;
 
       // In a traditional deck there should be exactly 4 Suits
       Assert.Equal(4, standardDeck.Select(x=>x.Suit).Distinct().Count());
       // There should also be 13 Cards per suit
       foreach(Suit suit in standardDeck.Select(x=>x.Suit).Distinct()){
-        var selectResults = standardDeck.Where(x=>x.Suit.Equals(suit)).ToList();
-        Assert.Equal(13, standardDeck.Where(x=>x.Suit.Equals(suit)).Count());
+        var suitCards = standardDeck.Where(x=>x.Suit.Equals(suit)).OrderBy(x=>x.Value).ToList();
+        Assert.Equal(13, suitCards.Count());
+        // For each suit there should be numeric cards 2-10 and face cards Jack, Queen, King & Ace
+        for(int i = 2; i < 11; i++){
+          Assert.NotEmpty(suitCards.Where(x=>x.Name == $"{i}"));
+        }
+        Assert.NotEmpty(suitCards.Where(x=>x.Name == "Jack"));
+        Assert.NotEmpty(suitCards.Where(x=>x.Name == "Queen"));
+        Assert.NotEmpty(suitCards.Where(x=>x.Name == "King"));
+        Assert.NotEmpty(suitCards.Where(x=>x.Name == "Ace"));
       }
+    }
+
+    [Fact]
+    public void TestDeckEquals()
+    {
+      // Test Same Order is Equals
+      var deck1 = Deck.BuildStandardDeck();
+      var deck2 = Deck.BuildStandardDeck();
+      Assert.True(deck1.Equals(deck2));
+    }
+
+    [Fact]
+    public void TestDeckNotEqualOnDifferentOrder()
+    {
+      // Different Order is not Equals
+      var deck1 = Deck.BuildStandardDeck();
+      var deck2 = Deck.BuildStandardDeck();
+      deck2.Shuffle();
+      Assert.False(deck1.Equals(deck2));
+    }
+
+    [Fact]
+    public void DeckNotEqualOnDifferentSizes()
+    {
+      // Different Count Not Equals
+      var deck1 = new Deck(new List<ACard>(){
+          new StandardCard(Suit.Hearts, 1, "One")
+          });
+      var deck2 = Deck.BuildStandardDeck();
+
+      Assert.False(deck1.Equals(deck2));
+    }
+
+    [Fact]
+    public void DeckNotEqualOnDifferentCards()
+    {
+      // Different Cards not Equals
+      var deck1 = new Deck(new List<ACard>(){
+          new StandardCard(Suit.Hearts, 1, "One")
+          });
+
+      var deck2 = new Deck(new List<ACard>(){
+          new StandardCard(Suit.Spades, 1, "One")
+          });
+      Assert.False(deck1.Equals(deck2));
     }
 }
